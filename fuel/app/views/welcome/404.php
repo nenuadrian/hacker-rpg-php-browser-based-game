3112640a2f47a3a6b1fac530b1913880194a7bc9
Adrian <adrian.nenu@gmail.com>
2016-11-27 23:53:05 +0000

    responsiveness

--- fuel/app/views/welcome/404.php
+++ fuel/app/views/welcome/404.php
@@ -4,7 +4,15 @@
 <p class="text-center">If you think something should be here, please let us know immediately!</p>
 <br/>
 <div class="text-center">
+<a href="<?php echo Uri::base(); ?>" class="btn btn-default">go to home</a>
+  <Br/><br/>
 <h1 style="font-size:150px">404</h1><Br/>
+<div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">
+<div class="videoWrapper">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/AT35iIWBSgE" frameborder="0" allowfullscreen></iframe>
+</div>
+</div>
 </div>
 <?php echo View::forge('global/footer'); ?>
