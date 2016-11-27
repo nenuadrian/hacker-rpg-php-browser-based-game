b7ede9fd667b8e334c31653d8b39dd2948ae145a
Adrian <adrian.nenu@gmail.com>
2016-11-27 16:20:40 +0000

    fix globaljs

--- fuel/app/views/knowledge/knowledge.php
+++ fuel/app/views/knowledge/knowledge.php
@@ -79,9 +79,9 @@ use \Model\Skills;
 
 
 
-<?php echo Asset::js('diamond-show/modernizr-custom.js'); ?>
-<?php echo Asset::js('diamond-show/snap.svg-min.js'); ?>
-<?php echo Asset::js('diamond-show/main.js'); ?>
+<?php echo GlobalJs::include('diamond-show/modernizr-custom.js'); ?>
+<?php echo GlobalJs::include('diamond-show/snap.svg-min.js'); ?>
+<?php echo GlobalJs::include('diamond-show/main.js'); ?>
 
 
 <?php echo View::forge('global/footer'); ?>
--- fuel/packages/globaljs/classes/globaljs.php
+++ fuel/packages/globaljs/classes/globaljs.php
@@ -3,12 +3,22 @@ namespace GlobalJs;
 
 class GlobalJs {
   private static $js = array();
+  private static $include_js = array();
 
   public static function add($js) {
     static::$js[] = $js;
   }
 
-  public static function render() {
-    return implode('', static::$js);
+  public static function include($js) {
+    static::$include_js[] = $js;
   }
+
+  public static function render() {
+    $output = "";
+    foreach(static::$include_js as $js) {
+      $output .= \Asset::js($js);
+    }
+    return $output . implode('', static::$js);
+  }
+
 }
