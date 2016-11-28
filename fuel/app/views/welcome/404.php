eeade8341aff8354a3f530ea0f68524d393bb8bb
Adrian <adrian.nenu@gmail.com>
2016-11-28 18:33:09 +0000

    ..

--- fuel/app/views/welcome/404.php
+++ fuel/app/views/welcome/404.php
@@ -4,7 +4,6 @@
 <p class="text-center">If you think something should be here, please let us know immediately!</p>
 <br/>
 <div class="text-center">
-<a href="<?php echo Uri::base(); ?>" class="btn btn-default">go to home</a>
   <Br/><br/>
 <h1 style="font-size:150px">404</h1><Br/>
 <div class="row">
