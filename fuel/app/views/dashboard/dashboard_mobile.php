b259a905b6c4e7b6481f1f05d49174eed8e1b371
Adrian <adrian.nenu@gmail.com>
2016-11-28 18:45:25 +0000

    social and design

--- fuel/app/views/dashboard/dashboard_mobile.php
+++ fuel/app/views/dashboard/dashboard_mobile.php
@@ -31,7 +31,20 @@ echo View::forge('global/header'); ?>
 <a href="https://www.facebook.com/theSecretRepublic" target="_blank"><i class="fa fa-facebook"></i></a>&nbsp;&nbsp;&nbsp;
 <a href="https://twitter.com/iSecretRepublic" target="_blank"><i class="fa fa-twitter"></i></a>&nbsp;&nbsp;&nbsp;
 <a href="https://www.youtube.com/user/TheSecretRepublicCom/" target="_blank"><i class="fa fa-youtube"></i></a>
-</h1>
+<!--
+<Br/>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.8&appId=1605215473026750";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-like" data-href="https://www.facebook.com/theSecretRepublic" data-layout="button_count" data-action="like"  data-show-faces="true" data-share="true"></div>
+--></h1>
+
 </div>
 </div>
 <?php echo View::forge('global/footer'); ?>
