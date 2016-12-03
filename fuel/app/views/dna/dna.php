86cc9c9a600570cd6f211ab47363ac00bfbe417c
Adrian <adrian.nenu@gmail.com>
2016-12-03 22:19:00 +0000

    fix

--- fuel/app/views/dna/dna.php
+++ fuel/app/views/dna/dna.php
@@ -4,10 +4,10 @@
 <div class="container text-center">
   <Br/><Br/>
     <div class="row">
-      <div class="col-sm-9">
+      <div class="col-sm-6">
         <div style="font-size:30px;padding-top:15px; padding-bottom:15px;">A.I. voice</div>
       </div>
-      <div class="col-sm-3">
+      <div class="col-sm-6">
         <form method="post">
           <button type="submit" class="btn btn-default btn-block" name="voice" value="true">
             <?php echo Auth::get('voice_enabled') ? 'enabled' : 'disabled'; ?>
