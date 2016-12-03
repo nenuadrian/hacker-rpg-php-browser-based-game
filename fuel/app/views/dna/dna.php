e4b5ca447e6697860da93ec10fb4282ad93a8a39
Adrian <adrian.nenu@gmail.com>
2016-12-03 22:02:11 +0000

    tt

--- fuel/app/views/dna/dna.php
+++ fuel/app/views/dna/dna.php
@@ -3,18 +3,18 @@
 
 <div class="container">
   <Br/><Br/>
-  <form method="post">
     <div class="row-fluid">
       <div class="col-xs-9">
         <h3>A.I. voice</h3>
       </div>
       <div class="col-xs-3 text-center">
-        <button type="submit" class="btn btn-default" name="voice" value="true">
-          <?php echo Auth::get('voice_enabled') ? 'enabled' : 'disabled'; ?>
-        </button>
+        <form method="post">
+          <button type="submit" class="btn btn-default" name="voice" value="true">
+            <?php echo Auth::get('voice_enabled') ? 'enabled' : 'disabled'; ?>
+          </button>
+        </form>
       </div>
     </div>
-  </form>
 </div>
 
 <?php echo View::forge('global/footer'); ?>
