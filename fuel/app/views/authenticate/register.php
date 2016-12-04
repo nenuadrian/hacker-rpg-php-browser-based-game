5e71f42b06afb2819efb87c9f07404fca10c2065
Adrian <adrian.nenu@gmail.com>
2016-12-04 01:32:20 +0000

    fix

--- fuel/app/views/authenticate/register.php
+++ fuel/app/views/authenticate/register.php
@@ -8,8 +8,8 @@
 		<?php echo View::forge('components/messages'); ?>
 
 		<form method="post" style="margin-top:50px">
-			<input type="text" autocapitalize="off" autocorrect="off"  class="form-control text-center" placeholder="user id" name="username" required value="<?php echo Input::post('username', ''); ?>" autocapitalize="off" autocorrect="off" />
-			<input type="email" autocapitalize="off" autocorrect="off"  class="form-control text-center" placeholder="e-mail" name="email" required value="<?php echo Input::post('email', ''); ?>" autocapitalize="off" autocorrect="off" />
+			<input type="text" class="form-control text-center" placeholder="user id" name="username" required value="<?php echo Input::post('username', ''); ?>" autocapitalize="off" autocorrect="off" autofocus="true" />
+			<input type="email" class="form-control text-center" placeholder="e-mail" name="email" required value="<?php echo Input::post('email', ''); ?>" autocapitalize="off" autocorrect="off" />
 			<input type="password" class="form-control text-center" placeholder="password" name="password" required value="<?php echo Input::post('password', ''); ?>" />
 			<br/>
 			<p>
