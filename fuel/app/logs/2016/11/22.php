1340ee4e6c58093c6be65a9427e3e159002b4214
Adrian <adrian.nenu@gmail.com>
2016-12-23 00:37:53 +0200

    fix

--- fuel/app/logs/2016/11/22.php
+++
@@ -1,5 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-ERROR - 2016-11-22 20:57:45 --> Fatal Error - Method Fuel\Core\View::__toString() must not throw an exception in /Applications/MAMP/htdocs/SecretAlpha/fuel/app/views/cardinal/mission/cardinal_quest_servers.php on line 0
-ERROR - 2016-11-22 20:58:00 --> Fatal Error - Method Fuel\Core\View::__toString() must not throw an exception in /Applications/MAMP/htdocs/SecretAlpha/fuel/app/views/cardinal/mission/cardinal_quest_servers.php on line 0
-ERROR - 2016-11-22 20:58:42 --> Fatal Error - Method Fuel\Core\View::__toString() must not throw an exception in /Applications/MAMP/htdocs/SecretAlpha/fuel/app/views/cardinal/mission/cardinal_quest_servers.php on line 0
