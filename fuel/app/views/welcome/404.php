f119a46286916866ff93a7f3056bff825dc9fca8
Nenu Adrian <nenuadrianmircea@yahoo.com>
2021-01-03 21:42:30 +0000

    Update 404.php

--- fuel/app/views/welcome/404.php
+++ fuel/app/views/welcome/404.php
@@ -5,13 +5,6 @@
 <br/>
 <div class="text-center">
   <Br/><br/>
-<h1 style="font-size:150px">404</h1><Br/>
-<div class="row">
-  <div class="col-md-4"></div>
-  <div class="col-md-4">
-<div class="videoWrapper">
-<iframe width="560" height="315" src="https://www.youtube.com/embed/AT35iIWBSgE" frameborder="0" allowfullscreen></iframe>
-</div>
-</div>
+<h1 style="font-size:150px">404</h1>
 </div>
 <?php echo View::forge('global/footer'); ?>
